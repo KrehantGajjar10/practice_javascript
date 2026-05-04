2527a94dd1222f9553bd784629c504d17f29107b
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-04 23:41:35 +0530

    Data Types

--- Practice1/practice1.js
+++ Practice1/practice1.js
@@ -1,2 +1,32 @@
-console.log(a);
-var a = 2;
+let a = 10;
+console.log(typeof a);
+
+let name = "Hello";
+console.log(typeof name);
+
+let b = true;
+console.log(typeof b);
+
+let x;
+console.log(typeof x);
+
+let y = null;
+console.log(y);
+console.log(typeof y);
+
+
+let  id1 = Symbol("id");
+let  id2 = Symbol("id");
+console.log(id1 === id2);
+
+let student = {
+    name : "abc",
+    age : 25,
+    isStudent : true
+};
+console.log(typeof student);
+console.log(student.name);
+
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(typeof fruits);
+console.log(fruits.length);
