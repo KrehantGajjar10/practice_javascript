ba8d809b5e89daefb26c6ed8892fcfc169e1a7af
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-06 23:58:38 +0530

    Switch Statement

--- Practice3/practice3.js
+++ Practice3/practice3.js
@@ -13,3 +13,33 @@ if (marks >= 90 && marks <= 100) {
 } else {
     console.log("Invalid marks");
 }
+
+// ------------------------
+
+let day = 7;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
