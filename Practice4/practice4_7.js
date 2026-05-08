d7762b2295d65839ff2c7ffa9d3f99b9719b5fae
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-08 23:44:45 +0530

    loop - ex-8

--- Practice4/practice4_7.js
+++ Practice4/practice4_7.js
@@ -1,5 +1,9 @@
+/*
+
 for(let i=1;i<51;i++){
     if(i%3 === 0){
         console.log(i);
     }
 }
+
+*/
---
+++ Practice4/practice4_8.js
@@ -0,0 +1,8 @@
+let val = prompt("Enter a number");
+for(let i=1;i<=val;i++){
+    if(i%2 === 0){
+        console.log(`${i} is even`);
+    } else {
+        console.log(`${i} is odd`);
+    }
+}
