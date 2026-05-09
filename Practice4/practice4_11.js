c400fa3d2c07938d08d89fdb45966834cde10523
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-09 15:25:37 +0530

    loop - ex-11

---
+++ Practice4/practice4_11.js
@@ -0,0 +1,11 @@
+countOdd = 0;
+countEven = 0;
+for(let i=1;i<=50;i++){
+    if(i%2 === 0){
+        countEven++;
+    } else {
+        countOdd++;
+    }
+}
+console.log(`Count of odd numbers: ${countOdd}`);
+console.log(`Count of even numbers: ${countEven}`);
