82480153c6a17df10e2dcca9f70a7d1398657eac
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-12 22:03:42 +0530

    Solve Sum of Digits Problem

--- Practice4/practice4_14.js
+++ Practice4/practice4_14.js
@@ -1,11 +1,11 @@
 /*
 
-let count = 0;
+let sum = 0;
 let number = prompt("Enter a number:");
 while(number > 0){
+    sum += number % 10;
     number = Math.floor(number / 10);
-    count++;
 }
-console.log(count);
+console.log(sum);
 
 */
