8d716a3df12d0c50bbd24e78c9a6f36288407eff
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-12 22:46:23 +0530

    Solve Palindrome Number Problem

---
+++ Practice4/practice4_15.js
@@ -0,0 +1,23 @@
+/*
+
+let number = 1221;
+let originalNumber = number;
+let reverse = 0;
+
+console.log(`Original number is: ${originalNumber}`);
+
+while(number > 0){
+    let digit = number % 10;
+    reverse = reverse * 10 + digit;
+    number = Math.floor(number / 10);
+}
+
+console.log(`Reverse of the number is: ${reverse}`);
+
+if(originalNumber === reverse){
+    console.log(originalNumber + " is a palindrome.");
+} else {
+    console.log(originalNumber + " is not a palindrome.");
+}
+
+*/
