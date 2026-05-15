1959dcaee774ab61ffdd2841c80d4ca0d67590ee
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-15 15:48:53 +0530

    Pass a function into another function and execute it inside.

---
+++ Practice5/practice5_2.js
@@ -0,0 +1,12 @@
+/*
+
+// Pass a function into another function and execute it inside.
+
+function ab(val){
+    val();
+}
+ab(function(){
+    console.log("Hello from a function passed as an argument!");
+});
+
+*/
