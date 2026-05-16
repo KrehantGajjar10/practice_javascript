9cbcd6f18cd263fbb809a5d6396126e42b55b29f
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-16 13:36:41 +0530

    Array : push , pop ,unshift , shift , splice , slice , sort ,reverse

---
+++ Practice6/practice6_2.js
@@ -0,0 +1,35 @@
+/*
+
+// Array : push , pop ,unshift , shift , splice , slice , sort ,reverse
+
+let arr = [80,20,30,40,50];
+
+
+arr.push(60);       // Pachad thi value add kare
+console.log(arr);
+
+arr.pop();          // Pachad thi value remove kare
+console.log(arr);
+
+arr.unshift(100);      // Agad thi value add kare
+console.log(arr);
+
+arr.shift();        // Agad thi value remove kare
+console.log(arr);
+
+arr.splice(2,3,90,100);     // splice value remove kare (index value(kyathi remove karvu chhe),value (right side ketli value remove karvi chhe),value(agad na 2 pachhi comma pachi thi value add thase (infinte)))
+console.log(arr);
+
+let newarr1 = arr.slice(2,4);    // slice value ne perticular part ma cut karine new array ma nakhi de
+console.log(newarr1);            // slice(index value(start),index value(end)(end value ni pahela vadi value suthi jase cut karse))
+
+let newarr2 = arr.sort(function(a,b){   //sort hamesha function use kare
+    return a - b ;                      // a - b => ascending order | b - a => descending order
+});
+console.log(newarr2);
+
+arr.reverse();              // reverse the array elements
+console.log(arr);
+
+
+*/
