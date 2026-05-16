f8d2f87095444ff949e086e15766f85ec7e2f48e
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-16 12:32:10 +0530

    Basic Array: Create,Access & Modify

---
+++ Practice6/practice6_1.js
@@ -0,0 +1,15 @@
+/*
+
+// Array : Create , access , Modify Arrays
+
+let arr = [10,20,30,40,50];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+
+arr[0] = 1000;
+console.log(arr[0]);
+
+*/
