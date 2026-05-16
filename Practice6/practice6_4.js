428c91b0868353d4163c602b91b30215b6aa7563
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-16 14:47:53 +0530

    Array : Destructuring & spread operator

---
+++ Practice6/practice6_4.js
@@ -0,0 +1,15 @@
+/*
+
+// Array : Destructuring & spread operator
+
+let arr = [10,20,30,40,50,60];      
+let [a,b, ,c] = arr;                // Assigning value to index -> Destructuring
+console.log(c);
+
+
+let arr2 = [...arr];                // Spread operator -> copy value into another array 
+arr2.pop();                         // not affect to original array
+console.log(arr);
+console.log(arr2);
+
+*/
