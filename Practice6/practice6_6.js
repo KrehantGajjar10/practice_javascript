164ac598c00ac8421ba8d53b3208cd902b85e639
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-17 16:10:17 +0530

    Keep the number greater than 10 using filter()

--- Practice6/practice6_6.js
+++ Practice6/practice6_6.js
@@ -9,5 +9,13 @@ let newarr1 = arr1.map(function (val) {
 console.log(newarr1);
 
 
+// Use .filter() to keep numbers greater than 10:
+let arr2 = [5, 12, 8, 20, 3];
+let newarr2 = arr2.filter((val) => {
+return val>10;
+});
+console.log(newarr2);
+
+
 
 */
