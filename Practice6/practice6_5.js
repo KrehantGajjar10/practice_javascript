b080f941fdf3f5f861cb3abe736b77d953e53ef3
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-17 15:57:51 +0530

    Extract Value from the Array using slice

--- Practice6/practice6_5.js
+++ Practice6/practice6_5.js
@@ -7,6 +7,10 @@ let colors = ["Green","Black","Yellow","Purple"];
 colors.splice(1,0,"Red","Blue");
 console.log(colors);
 
+// Extract only the middle 3 elements from this array
+let items = [1, 2, 3, 4, 5, 6];
+let arr1 = items.slice(2,5);
+console.log(arr1);
+
 
 */
-
