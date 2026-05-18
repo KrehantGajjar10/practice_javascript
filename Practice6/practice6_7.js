cd8e0d19a83e58104d79529669db8c50925548fc
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-18 21:42:20 +0530

    Use find() to get first number which is less than 10

--- Practice6/practice6_7.js
+++ Practice6/practice6_7.js
@@ -1,12 +1,20 @@
 /*
 
 // Use .reduce() to find the sum of this array:
-let arr = [10, 20, 30, 40];
+let arr1 = [10, 20, 30, 40];
 
-let sum=arr.reduce(function(acc, val){
+let sum=arr1.reduce(function(acc, val){
   return acc+val;
 }, 0);
 console.log(sum);
+
+// Use .find() to get the first number less than 10:
+let arr2 = [12, 15, 3, 8, 20];
+
+let ans = arr2.find(function (val) {
+  return val < 10;
+});
+console.log(ans);
 
 
 */
