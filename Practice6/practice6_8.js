9118c682111389a254e21f00eebcf25a4b09c336
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-18 21:45:19 +0530

    Use some() to check if any student has scored below 35

---
+++ Practice6/practice6_8.js
@@ -0,0 +1,10 @@
+/*
+
+// Use .some() to check if any student has scored below 35:
+let arr = [45, 60, 28, 90];
+let ans = arr.some(function (val) {
+  return val < 35;
+});
+console.log(ans);
+
+*/
