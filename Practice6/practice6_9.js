c9808c829809d66cbd98bf4c1b1621a7efc19c29
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-19 18:36:25 +0530

    Use Destructure to get firstName and lastName

---
+++ Practice6/practice6_9.js
@@ -0,0 +1,9 @@
+/*
+
+// Destructure this array to get firstName and lastName
+let fullName = ["Harsh", "Sharma"];
+let [firstName, lastName] = fullName;
+console.log(firstName);
+console.log(lastName);
+
+*/
