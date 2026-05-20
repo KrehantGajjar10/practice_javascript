e1bbf23af48a9b042ff2c6ed1c21d5868e8cabd0
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-20 23:22:07 +0530

    Merge two array using spread operator

--- Practice6/practice6_9.js
+++ Practice6/practice6_9.js
@@ -6,4 +6,11 @@ let [firstName, lastName] = fullName;
 console.log(firstName);
 console.log(lastName);
 
+// Merge two arrays using spread operator:
+let a = [1, 2];
+let b = [3, 4];
+
+let c = [...a, ...b];
+console.log(c);
+
 */
