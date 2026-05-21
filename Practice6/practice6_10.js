4afd5fac4be547e051d711c5ea41ac9098f3213f
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-21 19:22:52 +0530

    Clone the array (not by reference)

--- Practice6/practice6_10.js
+++ Practice6/practice6_10.js
@@ -1,5 +1,19 @@
+/*
+
+
 // Add "India" to the start of this array using spread:
 
 let arr = ["UK", "Russia", "USA"];
 arr = ["India", ...arr];
 console.log(arr);
+
+
+// Clone this array properly (not by reference):
+
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [...arr1];
+arr2.pop();
+console.log(arr1);
+console.log(arr2);
+
+*/
