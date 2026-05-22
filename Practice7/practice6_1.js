c39c9ab939897fe9e8342734067b9b5bf72f2dd6
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-22 19:13:53 +0530

    Object - Method-2 to access element

--- Practice7/practice6_1.js
+++ Practice7/practice6_1.js
@@ -1,3 +1,5 @@
+/*
+
 // Object : Two method to accese the value of object
 
 let obj = {
@@ -9,3 +11,7 @@ let obj = {
 console.log(obj);   // Print whole object
 console.log(obj.name);  // . find name in object and print it [First method]
 
+let a = "Food";
+console.log(obj[a]);    // Here a work as "food" and find in obj & print it
+
+*/
