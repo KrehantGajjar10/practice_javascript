4f6418cc06305bd1defc19600589a5a8f55ec6d6
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-23 15:12:38 +0530

    Object Destructuring

---
+++ Practice7/practice6_3.js
@@ -0,0 +1,24 @@
+/*
+
+
+// Object Destructuring
+
+const product = {
+  id: 101,
+  title: "Laptop",
+  specs: {
+    brand: "Apple",
+    model: "MacBook Air",
+    dimensions: {
+      weight: "1.24 kg",
+      screenSize: "13.6 inch"
+    }
+  },
+};
+
+let {weight,screenSize} = product.specs.dimensions;
+console.log(weight);
+console.log(screenSize);
+
+
+*/
