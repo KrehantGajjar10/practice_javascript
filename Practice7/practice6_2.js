d35ad4d2813977042b9f20e84cd018c75d49f810
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-23 15:07:56 +0530

    Nesting Object & Deep Access

---
+++ Practice7/practice6_2.js
@@ -0,0 +1,22 @@
+/*
+
+
+// Access nesting object/values & deep access
+
+const product = {
+  id: 101,
+  title: "Laptop",
+  specs: {
+    brand: "Apple",
+    model: "MacBook Air",
+    dimensions: {
+      weight: "1.24 kg",
+      screenSize: "13.6 inch"
+    }
+  },
+};
+console.log(product.specs.model);
+console.log(product.specs.dimensions.weight);
+
+
+*/
