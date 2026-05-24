1e9b2a9264f8a48592bfb73e9c526de14af95cf3
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-24 21:50:24 +0530

    Object Looping : for-in

---
+++ Practice7/practice6_4.js
@@ -0,0 +1,15 @@
+/*
+
+// Object Looping : for-in
+
+let obj = {
+    name: "Krehant",
+    age: 20,
+    email: "krehant@gmail.com",
+};
+
+for(let key in obj){
+    console.log(key, obj[key]);
+}
+
+*/
