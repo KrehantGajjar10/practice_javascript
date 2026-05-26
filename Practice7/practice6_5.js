f59e14338170b90932c6779444a8ab5affbf6b64
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-26 15:42:18 +0530

    Copying objects: spread

---
+++ Practice7/practice6_5.js
@@ -0,0 +1,10 @@
+// Copying objects: spread, Object.assign, deep clone
+
+let obj = {
+    name: "Krehant",
+    age: 20,
+    email: "krehant@gmail.com",
+};
+
+let obj2 = {...obj};
+console.log(obj2);
