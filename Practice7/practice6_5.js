4b41cc3710dcb5ff2c5f0a42dcab8280f90e0d14
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-26 16:08:34 +0530

    Copying objects: Deep Clone

--- Practice7/practice6_5.js
+++ Practice7/practice6_5.js
@@ -1,13 +1,25 @@
+/*
+
 // Copying objects: spread, Object.assign, deep clone
 
 let obj = {
     name: "Krehant",
     age: 20,
     email: "krehant@gmail.com",
+    address: {
+        city: "Ahmedabad",
+    },
 };
 
-let obj2 = {...obj};
+let obj2 = {...obj};            // spread
 console.log(obj2);
 
-let obj3 = Object.assign({ price: 100000}, obj);
+let obj3 = Object.assign({ price: 100000}, obj);    // Object.assign
 console.log(obj3);
+
+let obj4 = JSON.parse(JSON.stringify(obj));         // deep clone -> first object to string and then string to object 
+obj4.address.city = "Mumbai";                       // after we change the value of second one it don't affect original one
+console.log(obj);
+console.log(obj4);
+
+*/
