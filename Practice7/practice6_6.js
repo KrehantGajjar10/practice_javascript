6ede5a2fabf9b561e410f3f19f6fd029de21af12
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-27 15:11:45 +0530

    Optional Chaining

---
+++ Practice7/practice6_6.js
@@ -0,0 +1,12 @@
+// Optional chaining
+
+let obj = {
+    name: "Krehant",
+    age: 20,
+    email: "krehant@gmail.com",
+    addresses: {
+        city: "Ahmedabad",
+    },
+};
+console.log(obj?.address?.city);
+
