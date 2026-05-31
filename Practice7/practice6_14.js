94a24708fd69c691cb663984e51a8c8f47dc396c
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-01 00:03:38 +0530

    pr9 - use for-in to log all keys in object

--- Practice7/practice6_14.js
+++ Practice7/practice6_14.js
@@ -1,6 +1,10 @@
+/*
+
 // Destructure the key "first-name" as a variable called firstName.
 const user = {
     "first-name": "Krehant",
 };
 let { "first-name": firstName } = user;
 console.log(firstName);
+
+*/
