478c171d2d7b60fd6147753ffe42943e75385cb9
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-05-31 23:13:50 +0530

    pr6 - optional chaining problem

---
+++ Practice7/practice6_13.js
@@ -0,0 +1,16 @@
+/*
+
+// What will happen if coordinates is missing? How can you prevent errors?
+
+// Through optional chaining
+
+const locations = {
+  city: "Bhopal",
+  coordinate: {
+    lat: 23.2,
+    lng: 77.4,
+  },
+};
+console.log(locations?.coordinates?.lat);
+
+*/
