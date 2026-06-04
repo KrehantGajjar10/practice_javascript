0735c30d1be08fe124e42bf404877c3ff1a5f2f4
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-04 15:51:53 +0530

    Attribute manipulation: getAttribute, setAttribute, removeAttribute

---
+++ Part2ofjs/Practice1/practice1_3.js
@@ -0,0 +1,14 @@
+/*
+
+// Attribute manipulation: getAttribute, setAttribute, removeAttribute
+
+let a = document.querySelector("a");
+
+
+console.log(a.getAttribute("href"));
+
+a.setAttribute("href", "https://www.unsplash.com");
+
+a.removeAttribute("href");
+
+*/
