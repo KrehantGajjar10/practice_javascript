6b73999a735fed5bd14e29af4eac95dc5a5ffe5a
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-06 20:02:45 +0530

    Style Update via .classList

--- Part2ofjs/Practice1/practice1_5.js
+++ Part2ofjs/Practice1/practice1_5.js
@@ -1,6 +1,13 @@
 // Style Update via .style and classList (add, remove, toggle)
 
-let h2 = document.querySelector("h2");
+// let h2 = document.querySelector("h2");
+
+/*
 h2.style.color = "red";
 h2.style.fontFamily = "arial";
 h2.style.textTransform = "uppercase";
+*/
+
+// h2.classList.add("h2class");
+// h2.classList.remove("h2class");
+// h2.classList.toggle("h2class");
