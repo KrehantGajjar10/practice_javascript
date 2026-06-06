2441693ca30050907ba1ab4bc927f60be4174e6b
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-06 19:52:15 +0530

    Style Update via .style

--- Part2ofjs/Practice1/practice1_4.js
+++ Part2ofjs/Practice1/practice1_4.js
@@ -1,3 +1,5 @@
+/*
+
 // Dynamic DOM manipulation: createElement, appendChild, removeChild, prepend
 
 let h1 = document.createElement("h1");
@@ -6,3 +8,5 @@ h1.textContent = "Welcome - through createElement";
 document.body.append(h1);             // append means last ma add kare
 // document.body.prepend(h1);         // prepend means agad add kare
 // document.body.removeChild(h1);     // removeChild remove h1 in this example
+
+*/
---
+++ Part2ofjs/Practice1/practice1_5.js
@@ -0,0 +1,6 @@
+// Style Update via .style and classList (add, remove, toggle)
+
+let h2 = document.querySelector("h2");
+h2.style.color = "red";
+h2.style.fontFamily = "arial";
+h2.style.textTransform = "uppercase";
