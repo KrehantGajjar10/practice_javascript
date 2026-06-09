8868cb406d30481627b441c3afb6fa1ca17d92ca
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-10 02:10:09 +0530

    pr8 - Set the font size of all <p> elements to 48px using .style

---
+++ Part2ofjs/Practice1/practice1_12.js
@@ -0,0 +1,10 @@
+/*
+
+// Set the font size of all <p> elements to 48px using .style
+
+let p = document.querySelectorAll("p");
+p.forEach(function(elem){
+    elem.style.fontSize = "48px";
+})
+
+*/
