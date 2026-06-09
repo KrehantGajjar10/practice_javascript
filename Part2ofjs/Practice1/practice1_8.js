0be4dbafc23e2a235d5e3aed3e34ae1f8696594e
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-10 01:37:18 +0530

    pr4,5 - Remove attribute from the button and Create a new list item and add it to the end.

--- Part2ofjs/Practice1/practice1_8.js
+++ Part2ofjs/Practice1/practice1_8.js
@@ -5,3 +5,9 @@ let div = document.querySelector("div");
 div.setAttribute("title", "some info")
 
 */
+
+/*
+// Remove the disabled attribute from a button.
+let btn = document.querySelector("button");
+btn.removeAttribute("disabled");
+*/
