51f021343aa819cd9804c4d2f3dbbe3a014590f2
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-09 23:37:52 +0530

    pr2 - Select all <li> elements and print their text using a loop.

---
+++ Part2ofjs/Practice1/practice1_7.js
@@ -0,0 +1,14 @@
+/*
+
+// Select all <li> elements and print their text using a loop.
+
+  let lis = document.querySelectorAll("li");        // method 1
+  lis.forEach(function(val){
+    console.log(val.textContent);
+  })
+
+  for (let i = 0; i < lis.length; i++) {            // method 2
+    console.log(lis[i].textContent);
+  }
+
+  */
