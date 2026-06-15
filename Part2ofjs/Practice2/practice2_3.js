62b425dfdd69995e593b6de9d650790794d6c376
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-15 21:56:02 +0530

    Exercise : Keyboard Input Display

---
+++ Part2ofjs/Practice2/practice2_3.js
@@ -0,0 +1,15 @@
+/*
+
+// Exercise : Keyboard Input Display
+
+let h2 = document.querySelector("h2");
+
+window.addEventListener("keydown", function(dets){
+    if (dets.key === " ") {
+        h2.textContent = "SPC";
+    } else {
+        h2.textContent = dets.key;
+    }
+});
+
+*/
