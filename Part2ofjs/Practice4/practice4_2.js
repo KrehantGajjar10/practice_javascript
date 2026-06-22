036f0595b70c6f91769d5bf14f61699f79e21835
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-22 16:56:15 +0530

    Time & Intervals : setTimeout, clearTimeout, setInterval & clearInterval

---
+++ Part2ofjs/Practice4/practice4_2.js
@@ -0,0 +1,6 @@
+// setInterval & clearInterval
+let tm2 = setInterval(function(){       // repeatly run after 1s (1000 ms)
+    console.log("krehant");
+},1000);
+
+clearInterval(tm2);                     // clear setInterval
