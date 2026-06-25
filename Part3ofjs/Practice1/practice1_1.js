694a74a652437debff8af319bb010141d0599863
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-25 22:54:15 +0530

    Variable Scope: global, block, functional

---
+++ Part3ofjs/Practice1/practice1_1.js
@@ -0,0 +1,28 @@
+/*
+
+
+// Variable Scope: global, block, functional
+
+// function scope -> function ni ander j use thay
+// global scope -> aakha code ma gamme tya use kari sakiye
+// block scope -> aa only {} curly braces ma j use thay (like in if statement)
+
+// function scope
+function a() {
+  let w = 10;
+}
+
+// global scope
+let x = 20;
+
+// block scope
+if (true) {
+  let y = 30;
+}
+//Or
+{
+  let z = 40;
+}
+
+
+*/
