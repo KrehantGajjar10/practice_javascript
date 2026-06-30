c389d67ebccdade847ca885417dd7895f368a411
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-06-30 23:16:59 +0530

    Constructor functions and prototypes

---
+++ Part3ofjs/Practice3/practice3_1.js
@@ -0,0 +1,25 @@
+
+/*
+
+// Constructor functions and prototypes
+
+function CreatePencil(name, price, color, company) {        // Constructor
+  this.name = name;
+  this.price = price;
+  this.color = color;
+  this.company = company;
+}
+
+CreatePencil.prototype.write = function (text) {            // prototype -> badha instance par write automatically set thai jase
+  let h1 = document.createElement("h1");
+  h1.textContent = text;
+  h1.style.color = this.color;
+  console.log(h1);
+  document.body.append(h1);
+};
+
+let pencil1 = new CreatePencil("Nataraj", 10, "black", "nataraj");
+let pencil2 = new CreatePencil("Doms", 10, "red", "doms");
+
+
+*/
